4165f361e3eeb8fb4f65a5431faa0fbc049c1c5f
Chimpicus <caseyboyes@gmail.com>
2022-06-23 22:51:53 +0100

    rebranding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ interface FooterProps {}
 const Footer: React.FunctionComponent<FooterProps> = () => {
   return (
     <div id="footer" className="fixed b-0 w-full flex justify-center">
-      <h3>PIZZAPALACE</h3>
+      <h3>PapaSlice®</h3>
     </div>
   );
 };
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,12 @@ const Header: React.FunctionComponent<HeaderProps> = () => {
     <div className="bg-red-500 w-full text-slate-200 font-sansS p-5 flex justify-center">
       <div id="brandWHeader" className="text-center">
         <div id="brandTitle" className="">
-          <h1>PIZZA PALACE</h1>
+          <h1>Papa Slice®</h1>
         </div>
         <div id="subBrandTitle">
           <h2 className="text-sm">
             {" "}
-            <em> The home of premium woodfired pizzas </em>
+            <em> The home of giant premium woodfired pizzas!</em>
           </h2>
         </div>
       </div>{" "}
